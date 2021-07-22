34d239f5dd178368432877b33eb9b4c6fe0adb3f
mcartera47@gmail.com <mcartera47@gmail.com>
2021-07-22 15:07:47 +0300

    authcontext as a global component

--- SideEffecsProj/src/components/Home/Home.js
+++ SideEffecsProj/src/components/Home/Home.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import Card from '../UI/Card/Card';
-import Button from '../UI/Button/Button';
-import classes from './Home.module.css';
-
+import Card from "../UI/Card/Card";
+import Button from "../UI/Button/Button";
+import classes from "./Home.module.css";
+import AuthContext from "../../store/auth-context";
 const Home = (props) => {
+
+  const context = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button type="button" onClick={props.onLogout}>Log out</Button>
-    </Card> 
+      <Button type="button" onClick={context.onLogout}>
+        Log out
+      </Button>
+    </Card>
   );
 };
 
--- SideEffecsProj/src/store/auth-context.js
+++ SideEffecsProj/src/store/auth-context.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
- const AuthContext = React.createContext({
-    isLogged: false
+const AuthContext = React.createContext({
+  isLogged: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
 
-export default AuthContext; 
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userLoggedIn = localStorage.getItem("isLoggedIn");
+    if (userLoggedIn === "1") {
+      setLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    setLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    setLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+export default AuthContext;
