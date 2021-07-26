72ea9626a1ffe22d3e9a6c94d208a1b49c9aefe0
mcartera47@gmail.com <mcartera47@gmail.com>
2021-07-26 09:40:34 +0300

    fetching data when page loads

--- BackendGetStarted/src/App.js
+++ BackendGetStarted/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -6,32 +6,47 @@ import "./App.css";
 function App() {
   const [results, setResults] = useState([]);
   const [isLoading, setisLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function fetchMoiveHandler() {
+  const fetchMoiveHandler = useCallback(async () => {
     setisLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformmedData = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        realeaseDate: movieData.release_date,
-      };
-    });
-    setResults(transformmedData);
-    setisLoading(false);
-  }
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
 
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await response.json();
+
+      const transformmedData = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          realeaseDate: movieData.release_date,
+        };
+      });
+      setResults(transformmedData);
+    } catch (error) {
+      setError(error.message);
+    }
+    setisLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoiveHandler();
+  }, [fetchMoiveHandler]);
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoiveHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && results.length > 0 &&  <MoviesList movies={results} />}
-        {!isLoading && results.length === 0 &&  <p>Found no movies</p>}
+        {!isLoading && results.length > 0 && <MoviesList movies={results} />}
+        {!isLoading && results.length === 0 && !error && <p>Found no movies</p>}
         {isLoading && <p>Loading</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
