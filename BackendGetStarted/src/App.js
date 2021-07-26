fb90705e9b70e34913f33e5e96bbb1f98865dfb4
mcartera47@gmail.com <mcartera47@gmail.com>
2021-07-26 10:22:51 +0300

    sending push post

--- BackendGetStarted/src/App.js
+++ BackendGetStarted/src/App.js
@@ -13,22 +13,27 @@ function App() {
     setisLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://test-1d005-default-rtdb.firebaseio.com/movies.json"
+      );
 
       if (!response.ok) {
         throw new Error("Something went wrong");
       }
       const data = await response.json();
 
-      const transformmedData = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          realeaseDate: movieData.release_date,
-        };
-      });
-      setResults(transformmedData);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        return loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].realeaseDate,
+        });
+      }
+
+      setResults(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,13 +44,24 @@ function App() {
     fetchMoiveHandler();
   }, [fetchMoiveHandler]);
 
-  const addMovieHandler = (movie) => {
-    console.log(movie);
-  }
+  const addMovieHandler = async (movie) => {
+    const response = await fetch(
+      "https://test-1d005-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <React.Fragment>
       <section>
-        <AddMovie onClick={addMovieHandler} />{" "}
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
         <button onClick={fetchMoiveHandler}>Fetch Movies</button>
