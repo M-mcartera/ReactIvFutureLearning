152c8be0317b738139681834d3222e3a2ab386be
mcartera47@gmail.com <mcartera47@gmail.com>
2021-07-27 15:18:56 +0300

    redirect

--- MultiPageProj/src/pages/Welcome.js
+++ MultiPageProj/src/pages/Welcome.js
@@ -1,7 +1,12 @@
+import { Route } from "react-router-dom"
+
 const Welcome = () => {
   return (
     <div>
       <h1>Welcome Page</h1>
+      <Route path="/welcome/newUser">
+          <p>welcome new user</p>
+      </Route>
     </div>
   );
 };
