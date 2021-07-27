c5efb0f4faa038438cfb739f831aa6703b44b631
mcartera47@gmail.com <mcartera47@gmail.com>
2021-07-27 08:29:51 +0300

    handling error status and loading status

--- FoodOrderApp/src/components/Meals/AvailableMeals.js
+++ FoodOrderApp/src/components/Meals/AvailableMeals.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
 
 const AvailableMeals = () => {
-
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://test-1d005-default-rtdb.firebaseio.com/meals.json"
       );
+      if (!response.ok) {
+        throw new Error("something went wrong");
+      }
       const data = await response.json();
 
       const loadedMeals = [];
@@ -24,25 +28,46 @@ const AvailableMeals = () => {
           price: data[key].price,
         });
       }
-        setMeals(loadedMeals);
+      console.log(loadedMeals);
+      setMeals(loadedMeals);
+      setIsLoading(false);
     };
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+  if (httpError) {
+    return (
+      <section className={styles.httpError}>
+        <p>Something went wrong</p>
+      </section>
+    );
+  }
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      title={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={styles.meals}>
-      <Card>
-        <ul>
-          {meals.map((meal) => (
-            <MealItem
-              key={meal.id}
-              id={meal.id}
-              title={meal.name}
-              description={meal.description}
-              price={meal.price}
-            />
-          ))}
-        </ul>
-      </Card>
+      {meals.lenght < 0 || (
+        <Card>
+          <ul>{mealsList}</ul>
+        </Card>
+      )}
     </section>
   );
 };
